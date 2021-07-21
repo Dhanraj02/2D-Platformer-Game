8e249c071098a94f45e2d34d1d573a99319a1555
LAPTOP-MV5MSTRQ\Asus <dhanrajpepe123@gmail.com>
2021-07-21 22:39:59 +0530

    crouch movement

--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -5,30 +5,68 @@ using UnityEngine;
 public class playercontroller : MonoBehaviour
 {
     public Animator animator;
-    public float speed;
+
+    public float Speed;
+    public float jump;
+
+    private Rigidbody2D rb2d;
+    private BoxCollider2D boxCollider;
+
+    [SerializeField] private LayerMask platformLayerMask;
+
     private void Awake()
     {
-        Debug.Log("Player controller awake");
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        boxCollider = gameObject.GetComponent<BoxCollider2D>();
     }
 
     private void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
-        MoveCharacter(horizontal);
-        PlayMovementAnimation(horizontal);
+        float vertical = Input.GetAxisRaw("Jump");
+
+        PlayerMovementAnimation(horizontal, vertical);
+        MoveCharacter(horizontal, vertical);
+
+        // play crouch animation
+        if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+        {
+            
+            
+                animator.SetBool("courch", true);
+            
+        }
+        else
+        {
+            
+            
+                animator.SetBool("courch", false);
+            
+        }
 
     }
-    private void MoveCharacter(float horizontal)
-    {
-        Vector3 position = transform.position;
-        position.x += horizontal * speed * Time.deltaTime;
-        transform.position = position;
 
-    }
-    private void PlayMovementAnimation(float horizontal)
+    private void PlayerMovementAnimation(float horizontal, float vertical)
     {
-        animator.SetFloat("speed", Mathf.Abs(horizontal));
+        
+        
+            // Setting float value of speed inside animator
+            animator.SetFloat("speed", Mathf.Abs(horizontal));
+
+            // Setting value of 'jump' boolean
+            if (vertical > 0)
+            {
+                animator.SetBool("jump", true);
+            }
+            else
+            {
+                animator.SetBool("jump", false);
+            }
+        
+
+        // Change the direction of player
         Vector3 scale = transform.localScale;
+
         if (horizontal < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
@@ -37,19 +75,32 @@ public class playercontroller : MonoBehaviour
         {
             scale.x = Mathf.Abs(scale.x);
         }
+
         transform.localScale = scale;
-        //jump
-        Input.GetAxisRaw("Vertical");
-        Input.GetKeyDown(KeyCode.Space);
-        if(Input.GetKeyDown(KeyCode.Space))
+    }
+
+    private void MoveCharacter(float horizontal, float vertical)
+    {
+        // Horizontal character movement
+        Vector3 position = transform.position;
+        position.x += horizontal * Speed * Time.deltaTime;
+        transform.position = position;
+
+        // Vertical Character movement
+        if (vertical > 0 && IsGrounded())
         {
-            animator.SetBool("jump", true);
-        }
-        else
-        {
-            animator.SetBool("jump", false);
+            rb2d.AddForce(new Vector2(0f, jump), ForceMode2D.Force);
         }
     }
-}   
-    
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit;
+        float extraHeight = 0.3f;
+
+        raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, extraHeight, platformLayerMask);
+        return raycastHit.collider != null;
+    }
+}
+
 
