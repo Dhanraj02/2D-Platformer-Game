1597ccf53d6184ad601e287bb73fd19895225605
LAPTOP-MV5MSTRQ\Asus <dhanrajpepe123@gmail.com>
2021-07-21 19:26:48 +0530

    player movement

--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -5,22 +5,51 @@ using UnityEngine;
 public class playercontroller : MonoBehaviour
 {
     public Animator animator;
+    public float speed;
     private void Awake()
     {
-        Debug.Log("player controller awake");
+        Debug.Log("Player controller awake");
     }
+
     private void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("speed", Mathf.Abs(speed));
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        MoveCharacter(horizontal);
+        PlayMovementAnimation(horizontal);
+
+    }
+    private void MoveCharacter(float horizontal)
+    {
+        Vector3 position = transform.position;
+        position.x += horizontal * speed * Time.deltaTime;
+        transform.position = position;
+
+    }
+    private void PlayMovementAnimation(float horizontal)
+    {
+        animator.SetFloat("speed", Mathf.Abs(horizontal));
         Vector3 scale = transform.localScale;
-        if (speed < 0)
+        if (horizontal < 0)
         {
-            scale.x = -1f * scale.x;
-        }else if(speed > 0)
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if (horizontal > 0)
         {
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+        //jump
+        Input.GetAxisRaw("Vertical");
+        Input.GetKeyDown(KeyCode.Space);
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("jump", true);
+        }
+        else
+        {
+            animator.SetBool("jump", false);
+        }
     }
-}
+}   
+    
+
