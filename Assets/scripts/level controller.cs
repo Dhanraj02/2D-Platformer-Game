3816bed13621bd9449b4375f40199c5eb07d17d9
LAPTOP-MV5MSTRQ\Asus <dhanrajpepe123@gmail.com>
2021-07-29 12:16:04 +0530

    crouch script

---
+++ Assets/scripts/level controller.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelcontroller : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.GetComponent<playercontroller>() !=null)
+        {
+            Debug.Log("level finished by the player");
+        }
+    }
+}
--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -1,31 +1,73 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playercontroller : MonoBehaviour
 {
+    public scoreController scorecontroller;
     public Animator animator;
     public float speed;
+    public float jump;
+    private Rigidbody2D rb2d;
     private void Awake()
     {
-        Debug.Log("Player controller awake");
+        Debug.Log("player controller awake");
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
     }
 
+    public void killplayer()
+    {
+        Debug.Log("player killed by enemy");
+        Reloadlevel();
+    }
+
+    private void Reloadlevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void pickupkey()
+    {
+        Debug.Log("player picked up the key");
+        scorecontroller.IncreaseScore(10);
+    }
     private void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
-        MoveCharacter(horizontal);
-        PlayMovementAnimation(horizontal);
+        float vertical = Input.GetAxisRaw("Jump");
+        MoveCharacter(horizontal , vertical);
+        PlayMovementAnimation(horizontal , vertical);
+        bool crouch = Input.GetKey(KeyCode.LeftControl);
+        crouchAnimation(crouch);
 
     }
-    private void MoveCharacter(float horizontal)
+    private void MoveCharacter(float horizontal , float vertical)
     {
         Vector3 position = transform.position;
         position.x += horizontal * speed * Time.deltaTime;
         transform.position = position;
+        if(vertical > 0)
+        {
+            rb2d.AddForce(new Vector2(0f, jump), ForceMode2D.Force);
+        }
 
     }
-    private void PlayMovementAnimation(float horizontal)
+    private void crouchAnimation(bool crouch)
+    {
+        bool Crouch = Input.GetKey(KeyCode.LeftControl);
+        if(crouch)
+        {
+            animator.SetBool("crouch", true);
+        }
+        else
+        {
+            animator.SetBool("crouch", false);
+        }
+
+    }
+    private void PlayMovementAnimation(float horizontal, float vertical)
     {
         animator.SetFloat("speed", Mathf.Abs(horizontal));
         Vector3 scale = transform.localScale;
@@ -35,13 +77,15 @@ public class playercontroller : MonoBehaviour
         }
         else if (horizontal > 0)
         {
+
+
+
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
-        //jump
         Input.GetAxisRaw("Vertical");
         Input.GetKeyDown(KeyCode.Space);
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             animator.SetBool("jump", true);
         }
